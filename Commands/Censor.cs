cb9840ce87d9e40f0558fc94977fbfb6dd651e7f
Louis Prud'homme <lph@UNKNOWN.Home>
2021-07-17 14:26:27 +0100

    Added VSCode files to gititgnore Added overload for Delete command

--- Commands/Censor.cs
+++ Commands/Censor.cs
@@ -28,9 +28,31 @@ namespace Bishop.Commands
 
                 Task.WaitAll(futures.ToArray());
                 await origin.DeleteAsync();
-                await context.RespondAsync($"Removed {futures.Count + 1} 😉");
+                await context.RespondAsync($"Removed {futures.Count} 😉");
             }
             else await context.RespondAsync("You need to answer a message.");
+        }
+        
+        [Command("delete")]
+        [Description("Deletes the n last messages.")]
+        public async Task Delete(CommandContext context, int n)
+        {
+            if (n <= 0) 
+            {
+                await context.RespondAsync($"{n} is not a valid number."); // todo cleanify
+                return;
+            }
+            
+            var limit = context.Message;
+            var futures = context.Channel
+                .GetMessagesBeforeAsync(limit.Id).Result
+                .Take(n)
+                .Select(msg => msg.DeleteAsync())
+                .ToList();
+
+            Task.WaitAll(futures.ToArray());
+            await limit.DeleteAsync();
+            await context.RespondAsync($"Removed {futures.Count} 😉");
         }
     }
 }
