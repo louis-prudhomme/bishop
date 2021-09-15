b99d5a6e0a72e7b1b9e81fe2ee396e2553984692
Louis Prud’homme <louis.prudhomme@hotmail.com>
2021-09-15 14:09:08 +0200

    Yolo

--- Commands/Deleter.cs
+++ Commands/Deleter.cs
@@ -14,7 +14,7 @@ namespace Bishop.Commands
         [Command("delete")]
         [Aliases("d")]
         [Description("Deletes all the messages between the command and the one replied to.")]
-        public async Task Delete(CommandContext context)
+        public async Task Delete(CommandContext context, [RemainingText] string silentFlag)
         {
             if (context.Message.MessageType == MessageType.Reply)
             {
@@ -29,15 +29,14 @@ namespace Bishop.Commands
 
                 Task.WaitAll(futures.ToArray());
                 await origin.DeleteAsync();
+
+                if (silentFlag == null) return;
                 await context.RespondAsync($"Removed {futures.Count} 😉");
-            }
-            else
-            {
-                await context.RespondAsync("You need to answer a message.");
-            }
+            } else await context.RespondAsync("You need to answer a message.");
         }
 
-        [Command("delete")]
+        [Command("deleten")]
+        [Aliases("dn")]
         [Description("Deletes the n last messages.")]
         public async Task Delete(CommandContext context, int n)
         {
