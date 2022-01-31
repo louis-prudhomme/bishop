3454b76fe7ec7b92b17be6e5966071c1f2abe70a
Louis Prudʼhomme <louisph@bam.tech>
2022-01-31 12:40:06 +0100

    Minor change

--- Commands/Dump/Deleter.cs
+++ Commands/Dump/Deleter.cs
@@ -33,10 +33,7 @@ namespace Bishop.Commands.Dump
                 if (!string.IsNullOrEmpty(silentFlag)) return;
                 await context.RespondAsync($"Removed {futures.Count} 😉");
             }
-            else
-            {
-                await context.RespondAsync("You need to answer a message.");
-            }
+            else await context.RespondAsync("You need to answer a message.");
         }
 
         [Command("deleten")]
