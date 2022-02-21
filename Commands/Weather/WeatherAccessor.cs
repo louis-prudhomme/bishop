964ff334e8ed2328e55d4b42f72a650861145d17
Louis Prud’homme <louis.prudhomme@hotmail.com>
2022-02-21 22:12:57 +0100

    added restSharp + weather accessor

---
+++ Commands/Weather/WeatherAccessor.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using RestSharp;
+
+namespace Bishop.Commands.Weather;
+
+public class WeatherAccessor
+{
+    public static string _apiKey { private get; set; } = null!;
+    public static string _currentRoute { private get; set; } = "/v1/current.json";
+
+    private static readonly RestClientOptions Options = new("https://api.weatherapi.com/")
+    {
+        ThrowOnAnyError = true
+    };
+
+    private readonly RestClient _client = new(Options);
+
+    public async Task Current()
+    {
+        var request = new RestRequest()
+            .AddQueryParameter("key", _apiKey)
+            .AddQueryParameter("q", "Paris")
+            .AddQueryParameter("aqi", "no");
+
+
+        var response = await _client.ExecuteAsync(request);
+    }
+}
