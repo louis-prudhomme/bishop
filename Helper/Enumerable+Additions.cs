e9b4d5431aebcd0d912c91057de9ca2805b71824
Louis Prud'homme <louisph@bam.tech>
2022-07-14 10:34:09 +0200

    add extensions to easily join string enumerables

---
+++ Helper/Enumerable+Additions.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Bishop.Helper;
+
+public static class Enumerable
+{
+    public static string Join(this IEnumerable<string> source)
+    {
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
+        using var enumerator = source.GetEnumerator();
+
+        if (!enumerator.MoveNext())
+            throw new InvalidOperationException("no elements");
+
+        var acc = enumerator.Current;
+        while (enumerator.MoveNext())
+            acc = string.Join(acc, enumerator.Current);
+        return acc;
+    }
+    
+    public static string JoinWithNewlines(this IEnumerable<string> source)
+    {
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
+        using var enumerator = source.GetEnumerator();
+
+        if (!enumerator.MoveNext())
+            throw new InvalidOperationException("no elements");
+
+        var acc = enumerator.Current;
+        while (enumerator.MoveNext())
+            acc = string.Join("\n", acc, enumerator.Current);
+        return acc;
+    }
+}
