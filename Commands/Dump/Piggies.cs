746b6bcd832d7ad73810a0f4468adee64240c324
Louis Prud'homme <louisph@bam.tech>
2022-10-04 13:38:04 +0200

    :sparkles: (piggies) naive implementation

--- Commands/Dump/Piggies.cs
+++ Commands/Dump/Piggies.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Bishop.Helper.Extensions;
 using Bishop.Helper.Grive;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 
 namespace Bishop.Commands.Dump;
 
@@ -15,7 +18,12 @@ public class Piggies: BaseCommandModule
     [Description("Sends a random pigture.")]
     public async Task Random(CommandContext context)
     {
-        await context.RespondAsync(Walker.GetFiles(GriveDirectory.Pigtures).Random() ?? "<null>");
+        var p = Walker.GetFiles(GriveDirectory.Pigtures)!.Random();
+        if (p == null) return;
+        var z = new DiscordMessageBuilder();
+        await using var f = File.Open(p, FileMode.Open);
+        z.WithFile(f);
+        await context.RespondAsync(z);
     }
     
 }
